3c5ab1cfd22c145a02a8208ea230973107ed46ef
joannis-lbca <joannis.stavrakas@lbca.com.br>
2021-11-08 22:24:49 -0300

    ADD: Sklearn com e sem outros

--- sklearnBagofWords.py
+++ sklearnBagofWords.py
@@ -4,6 +4,7 @@ import sklearn as skl
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 #from sklearn import neighbors
+from sklearn.ensemble import RandomForestClassifier
 
 
 from sklearn.metrics import classification_report, plot_confusion_matrix
@@ -26,4 +27,18 @@ class SklBoW:
     # precision, recall, support and f1-score
     print(f'{classification_report(testY, predictions)}')
     plot_confusion_matrix(self.__model_LR, testX, testY)  
+    plt.show()
+    
+  def fitRandomForest(self, trainX, trainY):
+    self.__model_RF = RandomForestClassifier()
+    self.__model_RF.fit(trainX, trainY)
+    
+  def testRandomForest(self, testX, testY):
+    predictions = self.__model_RF.predict(testX)
+    score = self.__model_RF.score(testX, testY)
+    print(f'score:      { score*100:.3f}%')
+  
+    # precision, recall, support and f1-score
+    print(f'{classification_report(testY, predictions)}')
+    plot_confusion_matrix(self.__model_RF, testX, testY)  
     plt.show()
